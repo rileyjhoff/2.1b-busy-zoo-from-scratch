fef41a5ad637a71bc2c1503ce28189c8604c4373
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-28 16:47:50 -0700

    finished initial styling for fight scene

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -6,13 +6,13 @@ export default function Fight() {
     <div className='fight-container'>
       <div className='unicorn fighter'>
         <img alt='unicorn' src='unicorn.png' />
-        <Button >Power Up</Button>
-        <Button >Attack Lion</Button>
+        <Button variant='info'>Power Up</Button>
+        <Button variant='warning'>Attack Lion</Button>
       </div>
       <div className='lion fighter'>
         <img alt='unicorn' src='lion.png' />
-        <Button variant='outline-success'>Power Up</Button>
-        <Button variant='outline-danger'>Attack Unicorn</Button>
+        <Button variant='info'>Power Up</Button>
+        <Button variant='warning'>Attack Unicorn</Button>
       </div>
     </div>
   );
