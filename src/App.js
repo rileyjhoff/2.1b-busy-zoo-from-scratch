9bb652e671b8967417c19324634f31b1e6abaa85
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-28 16:01:10 -0700

    handle isOpen state in OpenSign. Move buttons from App to OpenSign component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import OpenSign from './components/OpenSign';
-import { Button } from 'react-bootstrap';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,20 +11,7 @@ function App() {
         <h1>Welcome to the Zoo</h1>
       </header>
       <main>
-        <div className='open-sign-container'>
-          <OpenSign isOpen={isOpen} />
-          {isOpen ? 
-            <div className='open-buttons'>
-              <Button variant="success" disabled>Open</Button>
-              <Button variant="danger" onClick={() => setIsOpen(false)}>Close</Button>
-            </div>
-            :
-            <div className='open-buttons'>
-              <Button variant="success" onClick={() => setIsOpen(true)}>Open</Button>
-              <Button variant="danger" disabled>Close</Button>
-            </div>
-          }
-        </div>
+        <OpenSign isOpen={isOpen} setIsOpen={setIsOpen} />
       </main>
       <footer>
         <h3>This is the footer</h3>
--- src/components/OpenSign.js
+++ src/components/OpenSign.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export default function OpenSign({ isOpen }) {
+export default function OpenSign({ isOpen, setIsOpen }) {
   return (
-    <div className='open-sign'>
+    <div className='open-sign-container'>
+      <div className='open-sign'>
+        {isOpen ? 
+          <img alt='open' src='/open.png' />
+          :
+          <img alt='closed' src='/closed.png' />
+        }
+      </div>
       {isOpen ? 
-        <img alt='open' src='/open.png' />
+        <div className='open-buttons'>
+          <Button variant="success" disabled>Open</Button>
+          <Button variant="danger" onClick={() => setIsOpen(false)}>Close</Button>
+        </div>
         :
-        <img alt='closed' src='/closed.png' />
+        <div className='open-buttons'>
+          <Button variant="success" onClick={() => setIsOpen(true)}>Open</Button>
+          <Button variant="danger" disabled>Close</Button>
+        </div>
       }
     </div>
   );
