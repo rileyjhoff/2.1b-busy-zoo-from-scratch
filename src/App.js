064c538106d43afd00cdb984acfa53f21f99a156
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-28 15:50:20 -0700

    added some styling to page

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <div className='open-sign-container'>
-        <OpenSign isOpen={isOpen} />
-        {isOpen ? 
-          <div className='open-buttons'>
-            <Button variant="success" disabled>Open</Button>
-            <Button variant="danger" onClick={() => setIsOpen(false)}>Close</Button>
-          </div>
-          :
-          <div className='open-buttons'>
-            <Button variant="success" onClick={() => setIsOpen(true)}>Open</Button>
-            <Button variant="danger" disabled>Close</Button>
-          </div>
-        }
-      </div>
+      <header>
+        <h1>Welcome to the Zoo</h1>
+      </header>
+      <main>
+        <div className='open-sign-container'>
+          <OpenSign isOpen={isOpen} />
+          {isOpen ? 
+            <div className='open-buttons'>
+              <Button variant="success" disabled>Open</Button>
+              <Button variant="danger" onClick={() => setIsOpen(false)}>Close</Button>
+            </div>
+            :
+            <div className='open-buttons'>
+              <Button variant="success" onClick={() => setIsOpen(true)}>Open</Button>
+              <Button variant="danger" disabled>Close</Button>
+            </div>
+          }
+        </div>
+      </main>
+      <footer>
+        <h3>This is the footer</h3>
+      </footer>
     </div>
   );
 }
