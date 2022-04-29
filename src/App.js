9773fcac69225a51fb1ca8637135a2b478b9f539
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-28 17:06:18 -0700

    finished Fight component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import OpenSign from './components/OpenSign';
 import Fight from './components/Fight';
 
 function App() {
+  const [unicornSize, setUnicornSize] = useState(10);
+  const [lionSize, setLionSize] = useState(10);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
@@ -12,7 +14,12 @@ function App() {
         <h1>Welcome to the Zoo</h1>
       </header>
       <main>
-        <Fight />
+        <Fight 
+          unicornSize={unicornSize} 
+          lionSize={lionSize} 
+          setUnicornSize={setUnicornSize}
+          setLionSize={setLionSize} 
+        />
         <OpenSign isOpen={isOpen} setIsOpen={setIsOpen} />
       </main>
       <footer>
--- src/components/Fight.js
+++ src/components/Fight.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-export default function Fight() {
+export default function Fight({ unicornSize, lionSize, setUnicornSize, setLionSize }) {
   return (
     <div className='fight-container'>
       <div className='unicorn fighter'>
-        <img alt='unicorn' src='unicorn.png' />
-        <Button variant='info'>Power Up</Button>
-        <Button variant='warning'>Attack Lion</Button>
+        <img alt='unicorn' src='unicorn.png' width={unicornSize * 10} />
+        {unicornSize < 20 ?
+          <Button variant='info' onClick={() => setUnicornSize(unicornSize + 1)} >Power Up</Button>
+          :
+          <Button variant='info' disabled>Power Up</Button>
+        }
+        {lionSize > 5 ?
+          <Button variant='warning' onClick={() => setLionSize(lionSize - 1)}>Attack Lion</Button>
+          :
+          <Button variant='warning' disabled>Attack Lion</Button>
+        }
       </div>
       <div className='lion fighter'>
-        <img alt='unicorn' src='lion.png' />
-        <Button variant='info'>Power Up</Button>
-        <Button variant='warning'>Attack Unicorn</Button>
+        <img alt='unicorn' src='lion.png' width={lionSize * 10} />
+        {lionSize < 20 ? 
+          <Button variant='info' onClick={() => setLionSize(lionSize + 1)}>Power Up</Button>
+          :
+          <Button variant='info' disabled>Power Up</Button>
+        }
+        {unicornSize > 5 ?
+          <Button variant='warning' onClick={() => setUnicornSize(unicornSize - 1)}>Attack Unicorn</Button>
+          :
+          <Button variant='warning' disabled>Attack Unicorn</Button>
+        }
       </div>
     </div>
   );
