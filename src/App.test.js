b16988236cf2ed77aa022827247ba1525f828550
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-18 17:06:07 -0700

    wrote test for open and close buttons disabled properties

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('close button disabled & open button NOT disabled on load. when I click the open button, the open button is disabled and the close button is no longer disabled. then, when I click on the close button, the close button is disabled and the open button is no longer disabled', () => {
   render(<App />);
-  const text = screen.getByText(/Welcome/i);
-  expect(text).toBeInTheDocument();
+  const openButton = screen.getByText(/Open/i);
+  const closeButton = screen.getByText(/Close/i);
+  expect(closeButton).toBeDisabled();
+  expect(openButton).not.toBeDisabled();
+  fireEvent.click(openButton);
+  expect(openButton).toBeDisabled();
+  expect(closeButton).not.toBeDisabled();
+  fireEvent.click(closeButton);
+  expect(closeButton).toBeDisabled();
+  expect(openButton).not.toBeDisabled();
 });
