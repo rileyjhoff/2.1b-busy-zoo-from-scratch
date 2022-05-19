d631538ce14ab441925d55eb9e991e1542416821
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-19 15:27:48 -0700

    refactored Parade to use CustomButton component instead of directly importing Button from mui

--- src/components/OpenSign.js
+++ src/components/OpenSign.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import CustomButton from './CustomButton';
 
 export default function OpenSign({ isOpen, setIsOpen }) {
--- src/components/Parade.js
+++ src/components/Parade.js
@@ -1,92 +1,91 @@
-import Button from '@mui/material/Button';
 import React from 'react';
 import Emoji from './Emoji';
+import CustomButton from './CustomButton';
 
 export default function Parade({ animals, setAnimals }) {
   return (
     <div className="parade-container">
       <h5>Add to the Animal Parade</h5>
       <div className="parade-buttons">
-        <Button
-          size="small"
-          variant="outlined"
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('rhino');
             setAnimals([...animals]);
           }}
-        >
-          🦏
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🦏'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('gorilla');
             setAnimals([...animals]);
           }}
-        >
-          🦍
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🦍'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('cheetah');
             setAnimals([...animals]);
           }}
-        >
-          🐆
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🐆'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('zebra');
             setAnimals([...animals]);
           }}
-        >
-          🦓
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🦓'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('elephant');
             setAnimals([...animals]);
           }}
-        >
-          🐘
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🐘'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('hippo');
             setAnimals([...animals]);
           }}
-        >
-          🦛
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🦛'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('tiger');
             setAnimals([...animals]);
           }}
-        >
-          🐅
-        </Button>
-        <Button
-          size="small"
-          variant="outlined"
+          text={'🐅'}
+        />
+
+        <CustomButton
+          size={'small'}
+          variant={'outlined'}
           onClick={() => {
             animals.push('giraffe');
             setAnimals([...animals]);
           }}
-        >
-          🦒
-        </Button>
+          text={'🦒'}
+        />
       </div>
       <div className="parade">
         <span>🚂</span>
